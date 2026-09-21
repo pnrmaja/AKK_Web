27dcda8498b844442acad7fc5e5085310ac41650
pnrmaja <ponauerszilvi1@gmail.com>
2026-09-21 08:46:14 +0200

    phpba át írás

--- index.php
+++ index.php
@@ -1,11 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php
+$oldalCim = 'Szalézi AKK';
+require __DIR__ . '/includes/header.php';
+?>
+
+    <h1>Szalézi Ágazati Képzőközpont</h1>
+    <p>Üdvözöljük a Szalézi Ágazati Képzőközpont oldalán!</p>
+
+<?php require __DIR__ . '/includes/footer.php'; ?>
